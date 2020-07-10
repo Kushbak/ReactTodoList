e866d7a1bd51c35f7a61e2587795f79346dd718b
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-10 20:17:37 +0600

    created component for Main page

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Main.module.css';
+import React from 'react';
+
+
+const Main = (props) => {
+    return (
+        <div className={styles.MainBlock}>
+            
+        </div>
+    )
+}
+
+
+export default Main;
