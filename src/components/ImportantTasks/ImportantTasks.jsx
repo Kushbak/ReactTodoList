6ca39de6ca9ff3d8296872c4a202bdd0d558b636
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-10 20:17:23 +0600

    created component for Important tasks

---
+++ src/components/ImportantTasks/ImportantTasks.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './ImportantTasks.module.css';
+
+
+const ImportantTasks = () => {
+    return(
+        <div className={styles.importantTasksBlock}>
+
+        </div>
+    )
+}
+
+export default ImportantTasks;
