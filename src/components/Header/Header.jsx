a7e317e6e1b5a9a5ec6eac6d9c1e8925ec0675bf
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-11 17:27:44 +0600

    added Logo and btn for change theme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,12 @@ import styles from './Header.module.css';
 const Header = (props) => {
     return(
         <div className={styles.headerBlock}>
-            <p className={styles.logo}>Todo<span>React</span></p>
-            <nav className={styles.navbar}>
-                
-            </nav>
+            <div className={styles.logo}>
+                <p className={styles.logo}>Todo List</p> 
+            </div>
+            <div className={styles.changeTheme}>
+                Change
+            </div>
         </div>
     )
 }
