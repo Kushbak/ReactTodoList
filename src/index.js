6d71f312bdae2cef20c0eb3f2bfd3023b45a4bf1
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-12 15:25:24 +0600

    added provider and store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; 
+import { createStore, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
+import { Provider } from 'react-redux';
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<Provider store={store}>
+				<App />
+			</Provider>
 		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
