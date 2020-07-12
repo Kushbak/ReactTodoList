0e2842677de8d84a826747ab7a449e49c5f5cc25
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-12 15:30:42 +0600

    added 2 components for display

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import styles from './Main.module.css';
-import React from 'react';
+import styles from './Main.module.css';  
+import TasksListContainer from './TasksList/TasksListContainer';
+import AddTaskContainer from './AddTask/AddTaskContainer';
 
 
 const Main = (props) => {
     return (
         <div className={styles.MainBlock}>
-            
+            <AddTaskContainer />
+            <TasksListContainer />
         </div>
     )
 }
