195ec41c02beffb1038a08449c1601c067e6941e
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-14 00:09:13 +0600

    added not completely ready function for adding task to important

--- src/components/Main/TasksList/TasksList.jsx
+++ src/components/Main/TasksList/TasksList.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import styles from './TasksList.module.css';
 
 const TasksList = (props) => {
+    let doImp = (e) => {
+        props.addToImportant()
+        console.log(e)
+    }
     return(
         <div className={styles.tasksListBlock}>
             {
-                props.tasks.map(t => <div className={styles.task} key={t.id}>
-                        <p className={styles.task__descr}>{t.descr}</p>
-                        {t.isImportant && <p>STAR</p>} 
+                props.tasks.map(t => (
+                    <div className={styles.task} key={t.id}>
+                            <p className={styles.task__descr}>{t.descr}</p>
+                            <button className={styles.doImp} onClick={ doImp }>imp</button>
+                            {t.isImportant && <p>STAR</p>} 
                     </div>
-                )
+                ))
             }
         </div>
     )
