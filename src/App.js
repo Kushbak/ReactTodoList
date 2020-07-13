e7da5ff0499cfe562942001b1f196460b5aae0e6
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-14 00:06:43 +0600

    changed names of component and added containers to display

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Main from './components/Main/Main';
-import ImportantTasks from './components/ImportantTasks/ImportantTasks';
-import { Route } from 'react-router-dom';
-import ArchiveTasks from './components/ArchiveTasks/ArchiveTasks';
+import Main from './components/Main/Main'; 
+import { Route } from 'react-router-dom'; 
+import ArchivedTasksContainer from './components/ArchivedTasks/ArchivedTasksContainer';
+import ImportantTasksContainer from './components/ImportantTasks/ImportantTasksContainer';
 
 
 const App = () => { 
@@ -15,8 +15,8 @@ const App = () => {
             <Navbar /> 
             <div className="main" >
                 <Route exact path='/' render={() => <Main />}/>
-                <Route path='/importantTasks' render={() => <ImportantTasks />}/>    
-                <Route path='/archiveTasks' render={() => <ArchiveTasks />}/>    
+                <Route path='/importantTasks' render={() => <ImportantTasksContainer />}/>    
+                <Route path='/archivedTasks' render={() => <ArchivedTasksContainer />}/>    
             </div>
         </div>
     );
