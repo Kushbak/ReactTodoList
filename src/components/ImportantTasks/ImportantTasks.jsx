75dfb2767c11eed676b2b6fcbb716a70db45156b
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-14 00:08:24 +0600

    added map for display tasks

--- src/components/ImportantTasks/ImportantTasks.jsx
+++ src/components/ImportantTasks/ImportantTasks.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import styles from './ImportantTasks.module.css';
 
 
-const ImportantTasks = () => {
+const ImportantTasks = (props) => {
     return(
         <div className={styles.importantTasksBlock}>
-
+            { props.importantTasks.map(t => (
+                <div className={styles.task} key={t.id}>
+                    <p className={styles.task__descr}>{t.descr}</p> 
+                </div>
+                    ))
+            }
         </div>
     )
 }
