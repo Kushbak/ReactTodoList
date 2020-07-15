47373cbc78d838fcad2a3f40700141ff693bcdfa
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-15 22:19:35 +0600

    changed to class component, added componentDidMount and added setTasks()

--- src/App.js
+++ src/App.js
@@ -4,22 +4,34 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Main from './components/Main/Main'; 
 import { Route } from 'react-router-dom'; 
-import ArchivedTasksContainer from './components/ArchivedTasks/ArchivedTasksContainer';
-import ImportantTasksContainer from './components/ImportantTasks/ImportantTasksContainer';
+import ArchivedTasks from './components/ArchivedTasks/ArchivedTasks';
+import ImportantTasks from './components/ImportantTasks/ImportantTasks';
+import { connect } from 'react-redux';
+import { setTasks } from './reducers/tasksReducer'
 
-
-const App = () => { 
-    return (
-        <div className="app" >
-            <Header />
-            <Navbar /> 
-            <div className="main" >
-                <Route exact path='/' render={() => <Main />}/>
-                <Route path='/importantTasks' render={() => <ImportantTasksContainer />}/>    
-                <Route path='/archivedTasks' render={() => <ArchivedTasksContainer />}/>    
+class App extends React.Component {  
+    componentDidMount(){
+        this.props.setTasks(1)
+    }
+    render(){
+        return (
+            <div className="app" >
+                <Header />
+                <Navbar /> 
+                <div className="main" >
+                    <Route exact path='/' render={() => <Main />}/>
+                    <Route path='/importantTasks' render={() => <ImportantTasks />}/>    
+                    <Route path='/archivedTasks' render={() => <ArchivedTasks />}/>    
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
-export default App;
+const mstp = (state) => {
+    return ({
+        tasks: state.tasksData.tasks
+    })
+}
+ 
+export default connect(mstp, { setTasks })(App);
