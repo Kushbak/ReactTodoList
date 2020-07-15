107e288f5a94fde041d2051a3c865e1732bf0452
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-15 22:17:59 +0600

    removed componentDiMount

--- src/components/Main/TasksList/TasksListContainer.jsx
+++ src/components/Main/TasksList/TasksListContainer.jsx
@@ -1,13 +1,9 @@
 import React from 'react'; 
 import TasksList from './TasksList';
 import { connect } from 'react-redux';
-import { addToImportant, setTasks } from '../../../reducers/tasksReducer';
+import { addToImportant } from '../../../reducers/tasksReducer';
 
 class TasksListContainer extends React.Component {
-    componentDidMount(){
-        this.props.setTasks(1)
-    }
-
     render() {
         return <TasksList {...this.props} />
     }
@@ -15,8 +11,8 @@ class TasksListContainer extends React.Component {
 
 const mstp = (state) => {
     return ({
-        tasks: state.tasksData.allTasks
+        tasks: state.tasksData.tasks
     })
 }
 
-export default connect(mstp, { addToImportant, setTasks })(TasksListContainer);
+export default connect(mstp, { addToImportant })(TasksListContainer);
