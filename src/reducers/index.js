d2369b0a98fff5809e2a772705029dcd3b8efeb0
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-16 22:49:35 +0600

    added usersReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from 'redux-form';
 import { tasksReducer } from "./tasksReducer";
+import { usersReducer } from "./usersReducer";
 
 
 
 export default combineReducers({
     form: formReducer,
-    tasksData: tasksReducer
+    tasksData: tasksReducer,
+    usersData: usersReducer
 })
