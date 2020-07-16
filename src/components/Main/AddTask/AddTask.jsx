9801b8ed180ea17c691886f90f127d2c639754f9
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-16 22:50:30 +0600

    changed object keys for submit to adding task

--- src/components/Main/AddTask/AddTask.jsx
+++ src/components/Main/AddTask/AddTask.jsx
@@ -3,22 +3,21 @@ import styles from './AddTask.module.css';
 import { Field, reduxForm } from 'redux-form';
 
 const AddTask = (props) => { 
-    const submitTask = (formData) => { 
-        let id = props.tasks.length;
-        let date = new Date();
-        let taskData = {
-            id,
-            date,
-            descr: formData.taskDescr,
-            isImportant: false
+    const submitTask = (formData) => {   
+        let taskData = {  
+            description: formData.taskDescr,
+            isImportant: formData.isImportant,
+            isArchived: false,
+            userId: 1
         }
-
+        
         props.addNewTask(taskData) 
     }
     return(
         <div className={styles.addTaskBlock}>
             <form onSubmit={props.handleSubmit(submitTask)} className={styles.addtTaskForm}>
-                <Field className={styles.inputTask} component='input' name='taskDescr' type='text' placeholder='What Do You Wanna Do?' />
+                <Field className={styles.inputTask} component='input' name='taskDescr' type='text' placeholder='What Do You Wanna Do?' /> 
+                <Field className={styles.isImportant} component='input' name='isImportant' type='checkbox' /> 
                 { props.error && <div className={styles.wrongData}>{ props.error }</div> }
                 <button className={styles.addBtn}>Add</button>
             </form>
