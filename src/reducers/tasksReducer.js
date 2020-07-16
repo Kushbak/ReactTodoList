a36477d3e8edda7034b5529eb6a45393b65a4ee8
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-16 22:49:25 +0600

    removed useless code

--- src/reducers/tasksReducer.js
+++ src/reducers/tasksReducer.js
@@ -1,16 +1,7 @@
 import { tasksApi } from "../api/api"
 
 let initialState = {
-    allTasks: [
-        // {id: 1, descr: 'Thats the first task for test1', date: '11.07.20 17:57', isImportant: false},
-        // {id: 2, descr: 'Thats the first task for test2', date: '17.07.20 17:57', isImportant: false},
-        // {id: 3, descr: 'Thats the first task for test3', date: '11.02.20 23:52', isImportant: true},
-        // {id: 4, descr: 'Thats the first task for test4', date: '17.07.20 17:57', isImportant: false},
-        // {id: 5, descr: 'Thats the first task for test5', date: '17.02.20 23:52', isImportant: true},
-        // {id: 6, descr: 'Thats the first task for test6', date: '11.02.20 23:52', isImportant: true}
-    ],
-    importantTasks: [],
-    archivedTasks: []
+    tasks: []
 }
 
 export const tasksReducer = (state = initialState, action) => {
@@ -18,7 +9,7 @@ export const tasksReducer = (state = initialState, action) => {
         case 'SET_TASKS': 
             return {
                 ...state,
-                allTasks: [...action.tasks]
+                tasks: [...action.tasks]
             } 
         case 'ADD_NEW_TASK': {
             let newTask = {
@@ -26,7 +17,7 @@ export const tasksReducer = (state = initialState, action) => {
             }
             return {
                 ...state,
-                allTasks: [...state.allTasks, newTask]    // либо можно этот кейс удалить и при создании нового таска перевызвать SET_TASKS
+                tasks: [...state.tasks, newTask]    // либо можно этот кейс удалить и при создании нового таска перевызвать SET_TASKS
             } 
         }
         case 'ADD_TO_IMPORTANT': 
@@ -77,8 +68,8 @@ export const setTasks = (userId) => (dispatch) => {
         })
 } 
 
-export const addNewTask = (userId) => (dispatch) => {
-    tasksApi.addTask(userId)
+export const addNewTask = (data) => (dispatch) => {
+        tasksApi.addTask(data)
         .then(res => {
             dispatch(addNewTaskSuccess(res.data))
         })
