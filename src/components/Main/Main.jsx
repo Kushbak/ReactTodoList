4c5497b8aba3a3a82370f01103b930f1144d7a35
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-17 21:38:40 +0600

    added useEffect for requesting tasks after login

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import styles from './Main.module.css';  
 import TasksListContainer from './TasksList/TasksListContainer';
 import AddTaskContainer from './AddTask/AddTaskContainer';
+import { useEffect } from 'react';
 
 
 const Main = (props) => {
+    useEffect(() => {
+        if(props.isAuth){
+            props.setTasks(props.userId)
+        }
+    })
     return (
         <div className={styles.MainBlock}>
             <AddTaskContainer />
