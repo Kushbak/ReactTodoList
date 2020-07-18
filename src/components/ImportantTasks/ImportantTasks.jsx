ab4f84fce305cba5c943c11019987c253ec435a4
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-18 21:58:20 +0600

    added filter for filtering important tasks

--- src/components/ImportantTasks/ImportantTasks.jsx
+++ src/components/ImportantTasks/ImportantTasks.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import styles from './ImportantTasks.module.css'
 
-class ImportantTasks extends React.Component {
+class ImportantTasks extends React.Component { 
     render() {
         return <div className={ styles.importantTasksBlock }>
-            { this.props.importantTasks.map(t => (
-                <div className={ styles.task } key={ t.id }>
-                    <p className={ styles.task__descr }>{ t.descr }</p>
-                </div>
-            )) }
+            { this.props.tasks
+                .filter(f => {
+                    return f.isImportant
+                })
+                .map(t => { 
+                    console.log(t);
+                    return(
+                        <div className={ styles.task } key={ t.id }>
+                            <p className={ styles.task__descr }>{ t.descr }</p>
+                        </div>
+                    )
+                }) 
+            }
         </div>
     }
 }
