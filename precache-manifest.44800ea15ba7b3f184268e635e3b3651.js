e70a5c9a9991bd9ec6298f849b34de8d6a111a6b
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-20 00:50:42 +0600

    Updates

---
+++ precache-manifest.44800ea15ba7b3f184268e635e3b3651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1786d05e932733aad17526bed02f05c",
+    "url": "/ReactTodoList/index.html"
+  },
+  {
+    "revision": "e064cb64ed2347f7fb68",
+    "url": "/ReactTodoList/static/css/main.6ceac5b2.chunk.css"
+  },
+  {
+    "revision": "269280a3fd553c573296",
+    "url": "/ReactTodoList/static/js/2.b64bf696.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactTodoList/static/js/2.b64bf696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e064cb64ed2347f7fb68",
+    "url": "/ReactTodoList/static/js/main.5d56bd2b.chunk.js"
+  },
+  {
+    "revision": "02579f7eebc518ff55ac",
+    "url": "/ReactTodoList/static/js/runtime-main.35d27f5c.js"
+  }
+]);
