12435246b11a4b7c6bf52eb90d86457e4947302e
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-22 00:29:03 +0600

    go back, changed baseURL to localhost

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import * as axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://my-json-server.typicode.com/kushbak/ReactTodoList/',
+    baseURL: 'http://localhost:8000/',
     headers: {
         "Content-type": "application/json; charset=UTF-8"
     }
