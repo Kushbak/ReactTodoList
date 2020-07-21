7e7e94c851ee1aa8dba451fd18ebf93a805d16fd
Kushbak Mamytov <mamytovkushbak@gmail.com>
2020-07-21 20:11:12 +0600

    added state hooks, added function for setting state for each action of input, added function for call request frunction

--- src/components/Main/TasksList/TasksList.jsx
+++ src/components/Main/TasksList/TasksList.jsx
@@ -1,39 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './TasksList.module.css'; 
-import ToggleProp from '../../common/ToggleProp/ToggleProp';
+import ToggleProp from '../../common/ToggleProp/ToggleProp'; 
+import { addToImportant, addToArchive, removeTask, editTask, doneTask, toggleEditMode } from '../../../reducers/tasksReducer';
+import { connect } from 'react-redux'; 
 
-const TasksList = (props) => {   
-    let currentInputValue = '';
-    const editInputValue = (taskId) => {
-        props.editTask(props.userId, taskId, currentInputValue)
-    }
+const TasksList = (props) => {
+    let [input, setInput] = useState();
+    // TODO: сделать editMode с помощью хуков, убрать editMode с апи
+
+    let setProps = (where, taskId, text, bool) => { 
+        props.tasks.map(item => {
+            if(item.id === taskId){
+                switch(where){
+                    case 'onChange':
+                        setInput(text);
+                        break;
+                    case 'onFocus':
+                        setInput(item.description);
+                        break;
+                    case 'editMode': 
+                        props.toggleEditMode(taskId, props.userId, bool) 
+                        break;
+                }
+            }
+        })
+    }     
+   
+    let sendUpdatedInput = (taskId) => {
+       props.editTask(props.userId, taskId, input)
+       setInput('');
+    }  
+ 
     return(
         <div className={styles.tasksListBlock}>
-            {props.tasks
+            { props.tasks
                     .filter(f => !f.isArchived )
                     .map(t => (
-                        <div className={styles.task} key={ t.id } >
-                            <div className={styles.valueBlock} >
+                        <div className={styles.task + ' ' + (t.isDone && styles.done) } key={ t.id } >
+                            <div className={styles.valueBlock} >    
                                 <input type="text" 
-                                    className={ styles.task__descr } 
-                                    value={ t.description }  
-                                    onChange={ (e) => { currentInputValue = e.target.value } } 
-                                    onBlur={ () => { editInputValue(t.id); this.setState({ editMode: false }) } }
-                                disabled /> 
+                                    className={ styles.task__descr + ' ' + (t.editMode && styles.task__editMode)  }  
+                                    value={ t.editMode ? (input || t.description) : t.description }  
+                                    onChange={ (e) => {  setProps('onChange', t.id, e.target.value, t.editMode) } } 
+                                    onFocus={ () => { setProps('onFocus', t.id, undefined, t.editMode) } } 
+                                    onBlur={ () => { sendUpdatedInput(t.id); setProps('editMode', t.id, undefined, false)} } 
+                                    disabled={ !t.editMode } /> 
 
                                 <ToggleProp { ...props } task={ t } />
                             </div>
                             
                             <div className={ styles.btnBlock }>
-                                <button onClick={ () => this.setState({ editMode: true }) }>EDIT</button>
+                                <button onClick={ () => props.doneTask(t.id, props.userId, !t.isDone) }>DONE</button>
+                                <button onClick={ () => setProps('editMode', t.id, undefined, true) }>EDIT</button>
                                 <button onClick={ () => props.removeTask(t.id, props.userId) } >REMOVE</button>
                             </div>
                         </div>
                     )
                 )
             }
-        </div>
+        </div>    
     )
+} 
+
+const mstp = (state) => {
+    return ({
+        tasks: state.tasksData.tasks,
+        userId: state.usersData.id
+    })
 }
 
-export default TasksList;
+export default connect(mstp, { addToImportant, addToArchive, removeTask, editTask, doneTask, toggleEditMode })(TasksList);
